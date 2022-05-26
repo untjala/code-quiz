316a094025955990747bc0773319f786c72dd7b9
Jala Unterreiner <jala.binbek@outlook.com>
2022-05-25 20:32:49 -0400

    created array to hold questions

--- Assets/script.js
+++ Assets/script.js
@@ -4,24 +4,25 @@ var landingEl = document.querySelector("#landing-page");
 var quizEl = document.querySelector("#quiz");
 var scoreEl = document.querySelector("#score-page");
 var beginBtnEl = document.querySelector("#landing-page button");
-var quizQuestionEl = document.querySelector("#question-page #question-header");
+var quizQuestionEl = document.querySelector("#question-page");
+var quizHeaderEl = document.querySelector("#question-header")
 var timerEl = document.querySelector("#countdown");
 var timeLeft = 60;
 var questions = document.querySelector("#questions");
 
 //Timer Duration Message
-function displayMessage () {
+function displayMessage() {
     timerEl.textContent = timeLeft + " seconds remaining"
 }
 //Funtion to run timer
 function startTimer() {
-    displayMessage ();
+    displayMessage();
     var timerInterval = setInterval(function () {
         timeLeft--;
-        displayMessage ();
-//Clears timer and sends end message
+        displayMessage();
+        //Clears timer and sends end message
         if (timeLeft === 0) {
-          timerEl.textContent = "Times up!";
+            timerEl.textContent = "Times up!";
             clearInterval(timerInterval)
         }
 
@@ -33,10 +34,34 @@ beginBtnEl.addEventListener("click", function (event) {
 });
 
 
-
-
+var quizQuestions = [
+    { 
+     question: "Sites and applications made with JavaScript cannot run on mobile devices:",
+     option: ["true", "false"],
+     answer: "true"
+    },
+    {
+        question: "i, when used in JavaScript, represents:",
+        option: ["indent", "index", "important"],
+        answer: "index"
+    },
+    {
+        question: "A for loop MUST contain an if statemnt:",
+        option: ["true", "false"],
+        answer: "false"
+    },
+    {
+        question: "A variable declared without a value will return:",
+        option: ["undefined", "unknown", "null"],
+        answer: "undefined"
+    },
+    {
+        question: "On it's own, (this) will refer to: ",
+        option: ["an undefined variable", "a specfic object", "the global object"],
+        answer: "the global object"
+    },
+];
 // WHEN I answer a question
-
 // THEN I am presented with another question
 
 // WHEN I answer a question incorrectly
