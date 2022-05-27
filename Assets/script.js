41c876cbe67458c21e50f8dd345c73dafbf59760
Jala Unterreiner <jala.binbek@outlook.com>
2022-05-26 20:36:05 -0400

    fixed function so questions display

--- Assets/script.js
+++ Assets/script.js
@@ -10,34 +10,36 @@ var quizQuestionsEl = document.querySelector("#questions");
 var options = document.querySelector("#options");
 var quizEl = document.querySelector("#quiz");
 var timeLeft = 60;
+var currentIndex = 0;
 var quizQuestions = [
     {
         title: "Sites and applications made with JavaScript cannot run on mobile devices:",
         option: ["true", "false"],
-        answer: "true"
+        correct: "true"
     },
     {
         title: "i, when used in JavaScript, represents:",
         option: ["index", "important"],
-        answer: "index"
+        correct: "index"
     },
     {
         title: "A for loop MUST contain an if statemnt:",
         option: ["true", "false"],
-        answer: "false"
+        correct: "false"
     },
     {
         title: "A variable declared without a value will return:",
         option: ["undefined", "unknown",],
-        answer: "undefined"
+        correct: "undefined"
     },
     {
         title: "On it's own, (this) will refer to: ",
         option: ["a specfic object", "the global object"],
-        answer: "the global object"
+        correct: "the global object"
     },
 ];
-var position = 0;
+
+
 //Timer Duration Message
 function displayMessage() {
     timerEl.textContent = timeLeft + " seconds remaining"
@@ -59,19 +61,41 @@ function startTimer() {
 }
 // WHEN I answer a question
 function displayQuestions() {
-    var currentQuestion = quizQuestions[position]; 
+    var currentQuestion = quizQuestions[currentIndex]; 
     var questionTitle = document.querySelector("#questions")
     questionTitle.textContent = currentQuestion.title
     currentQuestion.option.forEach(function(i)
      {
         var questionOptions = document.createElement("button")
         questionOptions.textContent = i
-        questionOptions.onclick = 
+        questionOptions.onclick = function(){
+            answerCheck();
+        }
         options.append(questionOptions)
-        console.log(currentQuestion.option, questionOptions, questionTitle)
-    })
+        })
+    function answerCheck (){
+        currentIndex++;
+        displayQuestions();
+        var userAnswer = quizQuestions.onclick
+        if (userAnswer === quizQuestions.correct); {
+            alert("Good Work!")
+        }
+    }   
+    }
+function displayEnd() {
+    if (timeLeft === 0) { 
+    welcomeEl.style.display = "none";
+    landingEl.style.display = "none";
+    quizHeaderEl.style.display = "none";
+};
 }
 
+
+function init() {
+    quizHeaderEl.style.display = "none";
+    submitBtnEl.style.display = "none";
+    scoreEl.style.display = "none";
+}
 //Begin button to start timer and quiz
 beginBtnEl.addEventListener("click", function (event) {
     startTimer();
@@ -80,39 +104,18 @@ beginBtnEl.addEventListener("click", function (event) {
     quizHeaderEl.style.display = "block";
     
 });
-
-function init() {
-    quizHeaderEl.style.display = "none";
-    submitBtnEl.style.display = "none";
-    scoreEl.style.display = "none";
-}
+submitBtnEl.addEventListener("click", function (event) {
+    event.preventDefault();
+    displayEnd();
+});
 
 // THEN I am presented with another question
-// questionEl.addEventListener("click", function (event) {
-//     var element = event.target;
-//     if (element.matches(quizQuestionsEl)) {
-//         position++;
-//         if (position < quizQuestions.length) {
-//             displayQuestions();
-//         } else {
-//             displayEnd();
-//         }
-//     }
-// });
-
 
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
 // WHEN all questions are answered or the timer reaches 
 // THEN the game is over 
-function displayEnd() {
-    scoreEl.style.display = "none";
-    submitBtnEl.style.display = "none";
-};
-submitBtnEl.addEventListener("click", function (event) {
-    event.preventDefault();
-    displayEnd();
-});
+
 
 // THEN I can save my initials and score 
 init();
